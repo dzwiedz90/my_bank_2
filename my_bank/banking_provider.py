32b03d339e50f343c8b64091e07970f1d59a6ba1
dzwiedz90 <upaciorek@gmail.com>
2019-07-05 12:35:20 +0200

    Created connection to db and filled with data(doesnt work)

--- my_bank/banking_provider.py
+++ my_bank/banking_provider.py
@@ -1,4 +1,5 @@
 import abc
+import sqlite3
 from typing import List
 
 
@@ -26,16 +27,90 @@ class AbstractBankingProvider(abc.ABC):
 
 
 class SQLiteBankingProvider(AbstractBankingProvider):
-    def __init__(self):
+    def __init__(self, database):
         self._logged_in = False
-        # self._name = ''
-        # self._surname = ''
-        # self._account_number = 0
-        # self._balance = 0
-        # self._login = ''
-        # self._password = ''
-        # self._address = ''
-        # self._pesel = 0
+        self._database = database
+
+        self._drop_accounts_table = 'DROP TABLE IF EXISTS accounts;'
+        self._create_accounts_table = """CREATE TABLE IF NOT EXISTS accounts 
+                                    (id INTEGER PRIMARY KEY ASC, 
+                                    account_number INTEGER NOT NULL, 
+                                    account_balance varchar(250) NOT NULL, 
+                                    users_id INTEGER NOT NULL, 
+                                    FOREIGN KEY(users_id) REFERENCES users(id))"""
+        self._drop_users_table = 'DROP TABLE IF EXISTS users;'
+        self._create_users_table = """CREATE TABLE IF NOT EXISTS users 
+                                    (id INTEGER PRIMARY KEY ASC, 
+                                    name varchar(250) NOT NULL, 
+                                    surname varchar(250) NOT NULL,
+                                    pesel BIGINT NOT NULL, 
+                                    address varchar(250) NOT NULL,
+                                    login_id INTEGER OT NULL,
+                                    account_id INTEGER NOT NULL,
+                                    FOREIGN KEY (login_id) REFERENCES logins(id),
+                                    FOREIGN KEY (account_id) REFERENCES accounts(id)    )"""
+        self._drop_logins_table = 'DROP TABLE IF EXISTS logins;'
+        self._create_logins_table = """CREATE TABLE IF NOT EXISTS logins 
+                                    (id INTEGER PRIMARY KEY ASC, 
+                                    login varchar(250) NOT NULL, 
+                                    password varchar(250) NOT NULL, 
+                                    users_id INTEGER NOT NULL, 
+                                    accounts_id INTEGER NOT NULL, 
+                                    FOREIGN KEY (users_id) REFERENCES users(id),
+                                    FOREIGN KEY (accounts_id) REFERENCES accounts(id))"""
+
+        conn = self.connect_db(self._database)
+        if conn is not None:
+            self.create_table(conn, self._drop_users_table)
+            self.create_table(conn, self._create_users_table)
+            self.create_table(conn, self._drop_accounts_table)
+            self.create_table(conn, self._create_accounts_table)
+            self.create_table(conn, self._drop_logins_table)
+            self.create_table(conn, self._create_logins_table)
+            conn.commit()
+        else:
+            print("Error! cannot create the database connection.")
+
+        accounts_data = (
+                          (None, '162472', '36589.99', '1'),
+                          (None, '244685', '12260.87', '2'),
+                          (None, '805054', '87329.63', '3'),
+                        )
+
+        users_data = (
+                        (None, 'Tomasz', 'Nowak', '53515564843', 'Poznań 60-320 Bukowska 1/2', '1', '1'),
+                        (None, 'Alfred', 'Szymczak', '74300625537', 'Gorzów Wlkp. 66-400 Stilonowa 13/8', '2', '2'),
+                        (None, 'Jan', 'Kowalski', '38300020761', 'Warszawa 01-020 Marszałowska 121/12', '3', '3')
+                     )
+
+        logins_data = (
+                        (None, '1234', '1234', '1', '1'),
+                        (None, '2345', '2345', '2', '2'),
+                        (None, '3456', '3456', '3', '3')
+                      )
+
+        self.insert_data_into_tables(conn, accounts_data, users_data, logins_data)
+
+    def connect_db(self, database):
+        try:
+            conn = sqlite3.connect(database)
+            return conn
+        except sqlite3.Error as e:
+            print(e)
+        return None
+
+    def create_table(self, conn, create_table_sql) -> None:
+        try:
+            c = conn.cursor()
+            c.execute(create_table_sql)
+        except sqlite3.Error as e:
+            print(e)
+
+    def insert_data_into_tables(self, conn, accounts_data: tuple, users_data: tuple, logins_data: tuple) -> None:
+        cur = conn.cursor()
+        cur.executemany('INSERT INTO accounts VALUES(?,?,?,?)', accounts_data)
+        cur.executemany('INSERT INTO users VALUES(?,?,?,?,?,?,?)', users_data)
+        cur.executemany('INSERT INTO logins VALUES(?,?,?,?,?)', logins_data)
 
     def log_in(self, user_id: int, user_pass: str) -> None:
         pass
@@ -53,4 +128,4 @@ class SQLiteBankingProvider(AbstractBankingProvider):
         pass
 
 
-x = SQLiteBankingProvider('dupa')
+x = SQLiteBankingProvider('resources/my_bank.db')
